aadbfbe3b1fa11d17b20def62efff96d0afc7628
luiscostalafc <luiscostalafc@gmail.com>
2021-10-26 12:36:15 -0300

    feat: create Header component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,8 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
 
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+
         </Head>
         <body>
           <Main />
