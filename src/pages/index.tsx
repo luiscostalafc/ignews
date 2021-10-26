cf20136fe25f1e62382d071a37547922173362f2
luiscostalafc <luiscostalafc@gmail.com>
2021-10-26 08:05:30 -0300

    feat: include sass in project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import styles from "../styles/home.modules.css";
+import styles from "../styles/home.module.scss";
 
 
 export default function Home() {
-  return <h1 style={styles.title}>Hellow Next</h1>;
+  return <h1 className={styles.title}>Hellow Next</h1>;
+
 }
