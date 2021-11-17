599a3c7153a7ba53732c009676cf05de1ddcbf4f
luiscostalafc <luiscostalafc@gmail.com>
2021-11-17 08:31:36 -0300

    feat: create webhooks

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -41,7 +41,17 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event;
 
     if (relevantsEvents.has(type)) {
-      // to do
+      try {
+        switch (type) {
+         case 'checkout.session.completed':
+           break;
+           default:
+             throw new Error('Unhandled event.')
+        }
+        
+      } catch (error) {
+        return res.json({ error: 'Webhook handler failde.'})
+      }
     }
 
     res.json({ receveid: true });
