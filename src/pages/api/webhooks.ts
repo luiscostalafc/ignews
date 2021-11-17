9ffd96b5ed9b7ec26a1ee7ba79b41b18fcf4dc1b
luiscostalafc <luiscostalafc@gmail.com>
2021-11-17 12:45:20 -0300

    feat: finished webhook

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -19,7 +20,13 @@ export const config = {
   },
 };
 
-const relevantsEvents = new Set(["checkout.session.completed"]);
+const relevantsEvents = new Set([
+  "checkout.session.completed",
+  "customer.subscription.created",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
@@ -43,7 +50,26 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relevantsEvents.has(type)) {
       try {
         switch (type) {
+        case 'customer.subscription.updated':
+        case 'customer.subscription.deleted':
+        
+        const subscription = event.data.object as Stripe.Subscription;
+
+        await saveSubscription(
+         subscription.id,
+         subscription.customer.toString(),
+        );
+
+          break;
          case 'checkout.session.completed':
+
+         const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+        await saveSubscription(
+          checkoutSession.subscription.toString(),
+          checkoutSession.customer.toString(),
+          true
+        )
            break;
            default:
              throw new Error('Unhandled event.')
